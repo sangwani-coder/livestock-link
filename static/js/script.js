88ed87799c6b93e0a2f47513e1c21d3dc85276ed
zyambo <mightypz2@gmail.com>
2023-04-02 16:28:49 +0200

    add bootstrap

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+// // Get the features container element
+// const featuresContainer = document.querySelector('.features-container');
+
+// // Get all the features
+// const features = document.querySelectorAll('.feature');
+
+// // Loop through each feature and set its height to match the container height
+// features.forEach((feature) => {
+//   feature.style.height = `${featuresContainer.offsetHeight}px`;
+// });
