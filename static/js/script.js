02b007066c50e5fa87a466cf906f54425ce6c7ff
zyambo <mightypz2@gmail.com>
2023-04-03 15:40:13 +0200

    Create vet/farmer dashboard pages

--- static/js/script.js
+++ static/js/script.js
@@ -8,3 +8,21 @@
 // features.forEach((feature) => {
 //   feature.style.height = `${featuresContainer.offsetHeight}px`;
 // });
+
+// toggle dark mode
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+    var elements = document.getElementsByClassName('card');
+    
+    for (var i=0, len=elements.length; i < len; i++){
+        elements[i].classList.toggle("dark-card");
+    }
+
+    var elements = document.getElementsByClassName('card-header');
+    for (var i=0, len=elements.length; i < len; i++){
+        elements[i].classList.toggle("dark-card-header");
+    }
+
+}
